a9dcfdeeea8ad58096a049e1c48e912af84ef6e1
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-04-10 11:38:55 +0300

    feat(project): Listings page, Cards components

---
+++ src/components/Listings/Cases/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Title from "../Title";
+import Case from "../Case";
+// styles
+import classNames from "classnames/bind";
+import styles from "./styles.module.scss";
+
+const cn = classNames.bind(styles);
+
+const Cases = ({ title }) => {
+  return (
+    <div className={cn('cases')}>
+      <Title text={title} />
+      <div className={cn("cases_cards")}>
+        <Case />
+        <Case />
+        <Case />
+        <Case />
+      </div>
+    </div>
+  );
+};
+
+export default Cases;
