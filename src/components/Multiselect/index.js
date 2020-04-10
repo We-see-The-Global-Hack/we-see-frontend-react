6a19162264857f4137d57676b923e6dfaa462ab7
Cheebam <fantastico@live.ur>
2020-04-10 19:30:02 +0300

    fix(project): fix multiselect

--- src/components/Multiselect/index.js
+++ src/components/Multiselect/index.js
@@ -1,42 +1,15 @@
 // react
-import React from "react";
+import React, { useCallback } from "react";
 import { Autocomplete } from "@material-ui/lab";
 import TextField from "@material-ui/core/TextField";
 
-const causes = [
-  "Education",
-  "Medical/Health",
-  "Gender Equality",
-  "Environmental/Animals protection",
-  "Cultural/Arts",
-  "Sports/Tourism/Recreation/Hobby",
-  "Social Services/Welfare/Poverty alleviation",
-  "Local Development/Communities",
-  "Support for Nonprofits or social business",
-  "Religion",
-  "International needs/Development aid",
-  "Law and rights Protection/Human Rights",
-  "Public affairs/Democracy/Advocacy",
-  "Labour Market/Professional affairs",
-  "Entrepreneurship/Income generation"
-];
-
-const targetAudience = [
-  "Children & Youth",
-  "Local Residents/Community",
-  "Women",
-  "Seniors",
-  "LGBT+",
-  "People with medical conditions or disabilities",
-  "Other Minorities",
-  "Animals",
-  "Other Nonprofits and social business"
-];
-
-const Multiselect = ({ name, label, ...rest }) => {
-  const options = name === "causes" ? causes : targetAudience;
-  console.log(name, rest);
-
+const Multiselect = ({ name, label, options, field, form, ...rest }) => {
+  console.log('form', form, name);
+  
+  const onChange = useCallback((event, values) => {
+    form.setFieldValue(name, values);
+  }, [name]);
+  
   return (
     <Autocomplete
       multiple
@@ -44,6 +17,7 @@ const Multiselect = ({ name, label, ...rest }) => {
       size="small"
       options={options}
       getOptionLabel={val => val}
+      onChange={onChange}
       renderInput={({...params}) => {
         return <TextField
           {...params}
