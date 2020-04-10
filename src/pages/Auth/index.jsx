9e2bd47a74db7e689c34b5fbd46a461159b750f1
Artem Gorbach <gorbacartem90@gmail.com>
2020-04-10 12:14:33 +0300

    add auth

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import SignIn from 'pages/Auth/SignIn';
 import SignUp from 'pages/Auth/SignUp';
+import logo from "assets/img/logo.svg"
 
 const Auth = () => {
   return (
+    <>
     <Switch>
       <Route path="/sign-in" component={SignIn} />
       <Route path="/sign-up" component={SignUp} />
       <Redirect to="/"/>
     </Switch>
+      </>
   );
 };
 
