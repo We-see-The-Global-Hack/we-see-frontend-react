dc1922ed9b38461607ebd229659f953ba9bf472c
DimaMakarenko <makarenko.d.viri@gmail.com>
2020-04-11 12:21:47 +0300

    Buttons link to search

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import { Redirect, Route, Switch } from 'react-router';
-import SignIn from 'pages/Auth/SignIn';
-import SignUp from 'pages/Auth/SignUp';
-import logo from "assets/img/logo.svg"
+import React from "react";
+import { Redirect, Route, Switch } from "react-router";
+import SignIn from "pages/Auth/SignIn";
+import SignUp from "pages/Auth/SignUp";
+import logo from "assets/img/logo.svg";
+import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+
+import classNames from "classnames/bind";
+import styles from "pages/Listings/styles.module.scss";
+
+const cn = classNames.bind(styles);
 
 const Auth = () => {
   return (
     <>
-    <Switch>
-      <Route path="/sign-in" component={SignIn} />
-      <Route path="/sign-up" component={SignUp} />
-      <Redirect to="/"/>
-    </Switch>
-      </>
+      <div className={cn('auth_search')}>
+        <div className={cn("listings_btn")}>
+          <Link
+            to="/search/offers"
+            className={cn("listings_link", " listings_link_first")}
+          >
+            <Button color="primary" variant="contained" size="small">
+              Search Offers
+            </Button>
+          </Link>
+          <Link to="/search/needs" className={cn("listings_link")}>
+            <Button color="primary" variant="contained" size="small">
+              Search Needs
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <Switch>
+        <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 };
 
--- src/pages/Listings/index.jsx
+++ src/pages/Listings/index.jsx
@@ -16,7 +16,6 @@ import dogTollImg from "assets/img/listings/image (4).png";
 import closesImg from "assets/img/listings/image (5).png";
 import mountainImg from "assets/img/listings/mountain.jpg";
 
-
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
 
@@ -34,8 +33,18 @@ const Listings = () => {
       <Title text="My Listings" />
       <div className={cn("listings")}>
         <div className={cn("listings_cases")}>
-          <Causes isOffers title="My Offers" link="/listings/offers" data={initialOffer} />
-          <Causes isNeeds title="My Needs" link="/listings/needs" data={initialNeeds} />
+          <Causes
+            isOffers
+            title="My Offers"
+            link="/listings/offers"
+            data={initialOffer}
+          />
+          <Causes
+            isNeeds
+            title="My Needs"
+            link="/listings/needs"
+            data={initialNeeds}
+          />
           <Causes title="My Matches" link="/listings" data={initialOffer} />
           <Causes
             title="Needs/Offers based on your interest"
@@ -45,13 +54,16 @@ const Listings = () => {
         </div>
         <div>
           <div className={cn("listings_btn")}>
-            <Link to="/search/offers" className={cn("listings_link listings_link_first")}>
-              <Button color="primary" variant="contained">
+            <Link
+              to="/search/offers"
+              className={cn("listings_link", " listings_link_first")}
+            >
+              <Button color="primary" variant="contained" size="small">
                 Search Offers
               </Button>
             </Link>
             <Link to="/search/needs" className={cn("listings_link")}>
-              <Button color="primary" variant="contained">
+              <Button color="primary" variant="contained" size="small">
                 Search Needs
               </Button>
             </Link>
@@ -72,7 +84,8 @@ const initialNeeds = [
     categoryName: "Material Goods",
     userId: "5e9046b052f75f19a56647b2",
     title: "I am offering my blue couch",
-    description: "I am offering my blue couch with pillows for those in need. It has been used, so there are some stains on it, but it will do for sleeping and chilling.\"\n",
+    description:
+      'I am offering my blue couch with pillows for those in need. It has been used, so there are some stains on it, but it will do for sleeping and chilling."\n',
     kind: "offer",
     estimatedQuantity: {
       value: 1,
@@ -86,7 +99,8 @@ const initialNeeds = [
     categoryName: "Agriculture & Animals' care",
     userId: "5e9046b052f75f19a56647b2",
     title: "Need someone to walk my dog",
-    description: "Hello, I need someone that can walk my dog for me. I am 70 years old, and I have issues with walking long distances, and I would need someone that can walk my dog 3 times a day.\n",
+    description:
+      "Hello, I need someone that can walk my dog for me. I am 70 years old, and I have issues with walking long distances, and I would need someone that can walk my dog 3 times a day.\n",
     kind: "need",
     image: dogImg,
     estimatedQuantity: {
@@ -100,7 +114,8 @@ const initialNeeds = [
     categoryName: "Groceries Help",
     userId: "5e9046b052f75f19a56647b2",
     title: "Help me get groceries",
-    description: "I have been tested with the virus, and I have been tested positive for the virus. I am a single woman living at home, and I cannot go to the supermarket anymore. Could someone help me get the groceries?",
+    description:
+      "I have been tested with the virus, and I have been tested positive for the virus. I am a single woman living at home, and I cannot go to the supermarket anymore. Could someone help me get the groceries?",
     kind: "need",
     image: groceriesImg,
     estimatedQuantity: {
@@ -114,7 +129,8 @@ const initialNeeds = [
     categoryName: "Looking for LGBT+ mentor",
     userId: "5e9046b052f75f19a56647b2",
     title: "Looking for LGBT+ mentor",
-    description: "I am a LGBT mentor that is looking for people to support. Please reach out to me if you need help",
+    description:
+      "I am a LGBT mentor that is looking for people to support. Please reach out to me if you need help",
     kind: "need",
     image: lgbtImg,
     estimatedQuantity: {
@@ -131,7 +147,8 @@ const initialOffer = [
     categoryName: "Food",
     userId: "5e9046b052f75f19a56647b2",
     title: "I am offering free meals",
-    description: "I am offering free meals to those who need it the most. Vegetarian and vegan options available, I am also able to deliver it for free to your door",
+    description:
+      "I am offering free meals to those who need it the most. Vegetarian and vegan options available, I am also able to deliver it for free to your door",
     kind: "offer",
     image: foodImg,
     estimatedQuantity: {
@@ -145,7 +162,8 @@ const initialOffer = [
     categoryName: "Toys",
     userId: "5e9046b052f75f19a56647b2",
     title: "Little dog toys",
-    description: "In my garage, I found this sweet little dog plushie. I don't use it anymore, and I think I can make someone happy with it. Feel free to ping me.",
+    description:
+      "In my garage, I found this sweet little dog plushie. I don't use it anymore, and I think I can make someone happy with it. Feel free to ping me.",
     kind: "offer",
     image: dogTollImg,
     estimatedQuantity: {
@@ -159,7 +177,8 @@ const initialOffer = [
     categoryName: "Clothes",
     userId: "5e9046b052f75f19a56647b2",
     title: "Free baby clothes",
-    description: "My baby cannot wear these clothing anymore, so I am giving these away for free. Clothing appropriate for 1-3 year old.\n",
+    description:
+      "My baby cannot wear these clothing anymore, so I am giving these away for free. Clothing appropriate for 1-3 year old.\n",
     kind: "offer",
     image: closesImg,
     estimatedQuantity: {
